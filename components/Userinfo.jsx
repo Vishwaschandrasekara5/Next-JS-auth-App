f42fb070c0edf2bf29b2b31f96bd573fee2df149
Vishwaschandrasekara5 <sandunv05@gmail.com>
2026-02-07 15:37:43 +0530

    userinfo page updated

--- components/Userinfo.jsx
+++ components/Userinfo.jsx
@@ -1,10 +1,22 @@
+'use client'; 
+
 import React from 'react'
 import SigninBtn from './SigninBtn'
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function Userinfo() {
-  return (
-    <div>
-      <SigninBtn/>
-    </div>
-  )
+    const {status, data:session} = useSession();
+
+    if (status === "authenticated"){
+        return(
+            <div className="shadow-xl p-8 rounded-md flex flex-col gap-3 bg-yellow-200">
+                <Image src={session?.user?.image} alt="User Image" width={60} height={60} className="rounded-full"/>
+                <h2 className="text-2xl font-bold mt-4">{session.user.name}</h2>
+                <p className="text-gray-600 font-bold">{session.user.email}</p>
+            </div>
+        )
+    }else{
+        return(<SigninBtn/>)
+    }
 }
