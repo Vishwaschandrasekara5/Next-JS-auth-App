b212d2ecec2c9b71753f7e876d0e0e05c7ca6772
Vishwaschandrasekara5 <sandunv05@gmail.com>
2026-02-07 15:12:44 +0530

    navbar updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,20 @@
+'use client';
+
 import React from 'react'
 import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Navbar() {
+    const {status} = useSession();
   return (
     <div className="p-4 flex justify-between items-center shadow-md">
       <Link className="font-bold text-lg text-blue-700" href={"/"}>VCoding</Link>
-      <button className="bg-slate-900 text-white px-4 py-2 rounded-md">Sign In</button>
+      {status === "authenticated" ? (
+        <button onClick={()=>signOut()} className="bg-slate-900 text-white px-4 py-2 rounded-md">Sign Out</button>
+      ) : (
+        <button onClick={()=>signIn("google")} className="bg-slate-900 text-white px-4 py-2 rounded-md">Sign In</button>
+      )}
+      
     </div>
   )
 }
