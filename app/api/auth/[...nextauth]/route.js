b7eb3a57dbe0b74c748eaab4f047ca28abeccda5
Vishwaschandrasekara5 <sandunv05@gmail.com>
2026-02-08 19:19:11 +0530

    userexists function updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,5 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -16,7 +18,11 @@ const authOptions = {
             if(account.provider === "google"){
                 const {name, email} = user;
                 try {
-                  const res =  await fetch("http://localhost:3000/api/user", {
+                    await connectMongoDB
+                    const userExists = await User.findOne({email});
+                 
+                    if(!userExists){
+                         const res =  await fetch("http://localhost:3000/api/user", {
                         method: "POST",
                         headers: {
                             "Content-Type": "application/json",
@@ -30,13 +36,14 @@ const authOptions = {
                     if(res.ok){
                          return user;
                     }
+                    }
                         
                 } catch (error) {
                     console.log(error);
                     
                 }
             }
-           
+           return user;
             
         }
     }
